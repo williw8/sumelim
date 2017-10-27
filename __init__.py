5917690c175b37234e8401faf204df533a0f7c3a
ww <winslow.williams>
2017-10-27 12:37:36 -0400

    Added license information

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+#    sumelim - bqcsv action module that eliminates rows or columns from a 
+#    CSV file based on whether or not the contents sum to a given threshold 
+#
+#    Copyright (C) 2017 Winslow Williams 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import wx
 from csvdb import csvmemory
 from actions import utils
